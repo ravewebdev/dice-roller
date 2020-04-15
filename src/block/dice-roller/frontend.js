2c0941d5d4e1296ac2678f09bf0e748cd5761c1c
R A Van Epps <rave@ravanepps.com>
2020-04-15 16:15:26 -0600

    Implement die rolling functionality

--- src/block/dice-roller/frontend.js
+++ src/block/dice-roller/frontend.js
@@ -1,7 +1,12 @@
-const rollerClass = '.wp-block-rave-dice-roller';
-const rollers = document.querySelectorAll( rollerClass );
+import icons from './icons';
 
-rollers.forEach( ( roller, index ) => {
+const { render } = wp.element;
+
+const wrapClass = '.wp-block-rave-dice-roller';
+const rollers = document.querySelectorAll( wrapClass );
+const rollingClass = 'rolling';
+
+rollers.forEach( ( roller ) => {
 	rollCurrentDice( roller );
 } );
 
@@ -11,7 +16,7 @@ rollers.forEach( ( roller, index ) => {
  * @author R A Van Epps <rave@ravanepps.com>
  * @since  1.0.0
  *
- * @param  {DOM element} roller Roller DOM element.
+ * @param  {Object} roller Roller DOM element.
  * @return {void}
  */
 function rollCurrentDice( roller ) {
@@ -20,4 +25,46 @@ function rollCurrentDice( roller ) {
 	if ( ! button.length ) {
 		return;
 	}
+
+	button[ 0 ].addEventListener( 'click', function() {
+		const dice = roller.querySelectorAll( '.dice-list .die-list' );
+
+		dice.forEach( animateDice );
+	} );
+}
+
+/**
+ * Animates all of current die type.
+ *
+ * Use timeout delays to display "rolling" animation.
+ *
+ * @author Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
+ * @since  1.0.0
+ *
+ * @param  {Object} dieList Wrapper node for current die list.
+ * @param  {number} index   Index of current die list.
+ */
+function animateDice( dieList, index ) {
+	setTimeout( function() {
+		dieList.classList.toggle( rollingClass );
+	}, 1000 * ( index ) );
+	setTimeout( function() {
+		const dieNum = parseInt( dieList.dataset.die.replace( 'd', '' ), 10 );
+		const dice = dieList.querySelectorAll( '.die' );
+
+		dice.forEach( ( die ) => {
+			const roll = Math.ceil( Math.random() * dieNum );
+			render(
+				<>
+					{ icons[ dieList.dataset.die ] }
+					<div className="die-roll">
+						{ roll }
+					</div>
+				</>,
+				die
+			);
+		} );
+
+		dieList.classList.toggle( rollingClass );
+	}, 1000 * ( index + 1 ) );
 }
